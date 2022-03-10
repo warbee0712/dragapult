92826099507b7e4ff75c21132d8455cc09420dda
Viktor De Pasquale <info@depasquale.wiki>
2022-03-10 14:55:50 +0100

    Added writer concat test

---
+++ dragapult-core/src/test/java/dragapult/DragapultWriterConcatTest.kt
@@ -0,0 +1,38 @@
+package dragapult
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.whenever
+
+class DragapultWriterConcatTest {
+
+    private lateinit var writer: DragapultWriterConcat<Any>
+
+    @Mock
+    private lateinit var second: DragapultWriter<Any>
+
+    @Mock
+    private lateinit var first: DragapultWriter<Any>
+
+    @BeforeEach
+    fun prepare() {
+        MockitoAnnotations.openMocks(this).close()
+        writer = DragapultWriterConcat(first, second)
+    }
+
+    @Test
+    fun `writes all resources`() {
+        val input = Any()
+        var firstWritten = false
+        var secondWritten = false
+        whenever(first.write(input)).thenAnswer { firstWritten = true; }
+        whenever(second.write(input)).thenAnswer { secondWritten = true; }
+        writer.write(input)
+        assertThat(firstWritten).isTrue()
+        assertThat(secondWritten).isTrue()
+    }
+
+}
